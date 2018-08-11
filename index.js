2fbe998e1db0bf37ecc7d231a77008dbfd9a0928
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-08-11 01:40:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ if(index.name === Joe) {
 console.log("The name is correct")
 }
 var height = "74"
+if(index.height === 74) {
+  console.log("The height is correct")
+}
  
 // Don't worry about this
 module.exports = { name, height
