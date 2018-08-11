d708c707d0be0ac2ce9cc60496cd1a4c96d7acbe
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-08-11 01:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var name = "Joe"
 if(index.name === Joe) {
 console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Joe")
 }
 var height = "74"
 if(index.height === 74) {
   console.log("The height is correct")
+} else {
+    console.log("Expected the name to be Joe")
 }
  
 // Don't worry about this
